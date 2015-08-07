e4bafc9f7dbb07218d3a1aa54b58307be537661f
Nicolas Payot <nicolas.payot@gmail.com>
2015-08-07 14:13:48 +0200

    chore(eslint): add eslint / babel-eslint parser / eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
-(function() {
-  'use strict';
+'use strict';
 
-  var webpack = require('webpack')
-  var path = require('path');
+var webpack = require('webpack')
+var path = require('path');
 
-  var app  = __dirname + '/app';
+var app  = __dirname + '/app';
 
-  module.exports = {
-    context: app,
-    entry: {
-      app: ['webpack/hot/dev-server', './index.js']
-    },
-    output: {
-      path: app + '/dist',
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loader: 'style!css!autoprefixer!sass'
-        },
-        {
-          test: /\.js$/,
-          loader: 'ng-annotate!babel',
-          exclude: /node_modules/
-        }
-      ]
-    }
-  };
-}());
+module.exports = {
+  context: app,
+  entry: {
+    app: ['webpack/hot/dev-server', './index.js']
+  },
+  output: {
+    path: app + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css!autoprefixer!sass'
+      },
+      {
+        test: /\.js$/,
+        loader: 'ng-annotate!babel!eslint',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
