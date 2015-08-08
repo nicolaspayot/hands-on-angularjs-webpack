97e445f09ab0dee07af3518106e5e75a660b7ff0
Nicolas Payot <nicolas.payot@gmail.com>
2015-08-08 12:49:13 +0200

    refactor(menu): use ng-cache-loader to import menu directive template

--- app/menu/menu.directive.js
+++ app/menu/menu.directive.js
@@ -1,8 +1,10 @@
+import menuHtml from './menu.html';
+
 function menu($timeout) {
   'ngInject';
   return {
     restrict: 'E',
-    templateUrl: './menu/menu.html',
+    templateUrl: 'menu.html',
     link: (scope, element) => {
       $timeout(1000).then(() => {
         element.find('ul').append('<li>Menu 4</li>');
