7edc3f9e83330050d37ba3c89cb92ce6cd0ef6da
Nicolas Payot <nicolas.payot@gmail.com>
2015-08-09 18:25:26 +0200

    chore(dist): add npm clean and dist scripts

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var app  = __dirname + '/app';
 module.exports = {
   context: app,
   entry: {
-    app: ['webpack/hot/dev-server', './index.js']
+    app: process.env.NODE_ENV === 'prod' ? './index.js' : ['webpack/hot/dev-server', './index.js']
   },
   output: {
     path: __dirname + '/dist',
@@ -16,9 +16,10 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.html$/, loader: "ng-cache-loader" },
+      { test: /\.html$/, loader: 'ng-cache' },
       { test: /\.scss$/, loader: 'style!css!autoprefixer!sass' },
       { test: /\.js$/, loader: 'ng-annotate!babel!eslint', exclude: /node_modules/ }
     ]
-  }
+  },
+  devtool: process.env.NODE_ENV === 'prod' ? 'source-map' : 'eval'
 };
